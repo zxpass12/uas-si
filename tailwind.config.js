5e068585b043643c8c74c47349fa9ef1d14ed4bf
ferdian satria <ferdiansatriafirdaus@gmail.com>
2025-11-23 14:52:06 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
